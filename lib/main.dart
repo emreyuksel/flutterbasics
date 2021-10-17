45812c25492ee1039ae5825b62d28b773affba41
emreyuksel <emreyuksel7@gmail.com>
2021-10-17 16:42:34 +0300

    Flutter öğrenme serüvenimin ilk commiti

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+// Main fonksiyonu app'in başlangıç noktasıdır.
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Center(
+        child: Text('Hello World'),
+      ),
+    ),
+  );
+}
