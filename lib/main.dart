78c53b2c0db2e99c8a662d80b5cfbad7de2a575b
emreyuksel <emreyuksel7@gmail.com>
2021-10-20 23:24:31 +0300

    Scaffold ve Widget çalışması yapıldı.

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,18 @@ import 'package:flutter/material.dart';
 // Main fonksiyonu app'in başlangıç noktasıdır.
 void main() {
   runApp(
-    const MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
+     MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: const Center(
+          child: Image(
+            image: NetworkImage('https://ichef.bbci.co.uk/news/640/cpsprodpb/16FA9/production/_92712149_gettyimages-480164327.jpg'),
+          ),
+        ),
       ),
     ),
   );
